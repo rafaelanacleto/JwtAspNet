7462c92ad77f9aa8705cb8d4fe45bad6c864e63f
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-12-21 11:31:26 -0300

    Initial commit

---
+++ Configuration.cs
@@ -0,0 +1,6 @@
+namespace JwtAspNet;
+
+public static class Configuration
+{
+    public static string PrivateKey { get; set; } = "abacaxi_2023*";
+}
---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+namespace JwtAspNet.Models;
+
+public record User(
+    int Id,
+    string Email,
+    string Password,
+    string[] Roles
+);
